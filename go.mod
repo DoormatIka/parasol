5c25ed051d7cc6fae33cdb5af1395319a9701873
DoormatIka <trashcanartistlily@gmail.com>
2024-06-17 00:43:55 +0800

    moved to discordgo

--- go.mod
+++ go.mod
@@ -2,11 +2,10 @@ module parasol/main
 
 go 1.22.4
 
-require github.com/diamondburned/arikawa/v3 v3.3.6
+require github.com/bwmarrin/discordgo v0.28.1
 
 require (
-	github.com/gorilla/schema v1.3.0 // indirect
-	github.com/gorilla/websocket v1.5.1 // indirect
-	golang.org/x/net v0.24.0 // indirect
-	golang.org/x/time v0.5.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
 )
--- main.go
+++ main.go
@@ -3,20 +3,30 @@ package main
 import (
 	"log"
 	"os"
+	"os/signal"
+	"regexp"
+	"strings"
+	"syscall"
 
-	// "github.com/diamondburned/arikawa/v3/discord"
-	"github.com/diamondburned/arikawa/v3/gateway"
-	"github.com/diamondburned/arikawa/v3/utils/bot"
-)
+	"github.com/bwmarrin/discordgo"
+);
 
-type Bot struct {
-	Ctx *bot.Context
+var command_regex, err = regexp.Compile(`\?\?(\w+)\s(.+)`);
+
+type Commands struct {};
+
+func (p *Commands) RunCommand(s string) {
+	matches := command_regex.FindStringSubmatch(`??quit args1 arg5`);
+	if matches != nil {
+		command := matches[1];
+		args := strings.Split(matches[2], " ");
+		// how do i get the command name and activate the ping command?
+		// my idea rn is to make a map[string]func and just do a get(), but there has to be a better way?
+	}
 }
-func (bot *Bot) Ping(*gateway.MessageCreateEvent) (string, error) {
-	return "Pong!", nil;
-}
-func (bot *Bot) Help(*gateway.MessageCreateEvent) (string, error) {
-	return bot.Ctx.Help(), nil;
+
+func (c *Commands) Ping(s *discordgo.Session, msg *discordgo.MessageCreate) {
+	
 }
 
 // To run, do `BOT_TOKEN="TOKEN HERE" go run .`
@@ -24,14 +34,41 @@ func (bot *Bot) Help(*gateway.MessageCreateEvent) (string, error) {
 // set BOT_TOKEN=TOKEN_HERE
 // go run .
 func main() {
-	token := os.Getenv("BOT_TOKEN");
-	if token == "" {
-		log.Fatalln("No BOT_TOKEN given. Do `BOT_TOKEN=\"TOKEN HERE\" go run .`");
+	if err != nil {
+		log.Fatalln("Regex:", err);
 	}
 
-	commands := &Bot{};
-	bot.Run(token, commands, func(ctx *bot.Context) error {
-		ctx.HasPrefix = bot.NewPrefix("!!");
-		return nil;
-	});
+	token := os.Getenv("BOT_TOKEN");
+	if token == "" {
+		log.Fatalln("No token provided.");
+	}
+	dg, err := discordgo.New("Bot " + token);
+	if err != nil {
+		log.Fatalln("Error opening connection,", err);
+	}
+	dg.AddHandler(MessageCreate);
+	dg.Identify.Intents = discordgo.IntentsGuildMessages | discordgo.IntentMessageContent;
+
+	if err := dg.Open(); err != nil {
+		log.Fatalln("Error oppening connection,", err);
+	}
+	log.Println("Bot is now running. Press CTRL-C to exit.");
+	sc := make(chan os.Signal, 1);
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill);
+	<-sc // blocks main loop until sc recieves something, which are these syscalls.
+
+	// cleanly closes out the discord sesh
+	dg.Close();
+}
+
+func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.Bot {
+		return;
+	}
+	if m.Content == "!!ping" {
+		_, err := s.ChannelMessageSendReply(m.ChannelID, "Pyon.", m.Reference());
+		if err != nil {
+			log.Println("Ping failed to send reply.", err);
+		}
+	}
 }
